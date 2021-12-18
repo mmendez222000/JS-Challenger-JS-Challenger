69c7acecaf9c653fb9d3c946eff799e6cc44a525
Mario <mmendez222000@gmail.com>
2021-12-18 15:41:07 -0600

    Cambio 2

--- CaracteresConsecutivos/main.js
+++ CaracteresConsecutivos/main.js
@@ -1,20 +1,28 @@
-let arr = "abbcccddddeeeeedcba";
-let arr2 = [];
-let contador = 1;
+const cadena = "abbcccddddeeeeeeedcba";
 
-for(let i=0; i < arr.length; i++){
-  const letraActual = arr[i];
-  const LetraSig = arr[i+1]; 
-  
-  if(letraActual === LetraSig){
-    contador++
-    arr2.push(contador);
-  }else{
-    contador = 1;
+function valdiar(arr){
+  let arr2 = [];
+  let contador = 1;
+  let resultado;
+
+  for(let i=0; i < arr.length; i++){
+    const letraActual = arr[i];
+    const LetraSig = arr[i+1]; 
+    
+    if(letraActual === LetraSig){
+      contador++
+      arr2.push(contador);
+    }else{
+      contador = 1;
+    }
   }
+
+  resultado = arr2.reduce((prev, item)=>{return Math.max(prev,item)});
+
+  document.write(resultado);
+  console.log(resultado);
 }
 
-let resultado = arr2.reduce((prev, item)=>{return Math.max(prev,item)});
+valdiar(cadena);
 
-document.write(resultado);
-console.log(resultado);
+
---
+++ SubCadenaPalindromo/main.js
@@ -0,0 +1,23 @@
+function valor(Pcadena){
+    
+    let cadena =  Pcadena.split("").reverse().join("");
+    let valor1 = '';
+    let valor2 = '';
+
+    for(i = 0; i < Pcadena.length; i++) { 
+
+        valor1 = Pcadena.substring(0,Pcadena.length - i);
+        valor2 = cadena.substring(0 + i,cadena.length);
+    
+        if (valor1 === valor2){
+            return valor1;            
+        }else if(Pcadena.substring(i,i+1) === Pcadena.substring(i+1,i+2)){
+            return Pcadena.substring(i,i+1) + Pcadena.substring(i+1,i+2);
+        }
+    }
+}
+
+console.log(valor("babad"));
+//console.log(valor("cbbd"));
+//console.log(valor("a"));
+//console.log(valor("ac"));
